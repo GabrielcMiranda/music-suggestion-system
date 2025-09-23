c5407c2e26ebd5b924918ce36bf7bc7744cbf21b
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-09-23 01:00:11 -0300

    feat: add user and recommendation models to project

---
+++ backend/app/model/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, UUID, String, JSON, BigInteger, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ ='user'
+    id = Column(UUID, primary_key=True)
+    username = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    favorite_music_genre = Column(String)
+    recommendations = relationship('Recommendation', backref='user')
+
+class Recommendation():
+    __tablename__ = 'recommendation'
+    id = Column(BigInteger, primary_key= True, autoincrement=True)
+    user_id = Column(UUID, ForeignKey('user.id'), nullable= False)
+    musicas = Column(JSON, nullable=False)

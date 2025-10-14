84a5f6a85e04d346cdc69b60502b29252531a519
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-10-14 15:21:15 -0300

    feat: add api and auth services

---
+++ frontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private tokenKey = 'access_token';
+  
+  constructor(private api: ApiService){}
+
+  login(loginData: {login:string, password:string}){
+    return this.api.post<{access_token:string, token_type:string}>('auth/login', loginData).pipe(
+      tap(response => {
+        localStorage.setItem(this.tokenKey, response.access_token);
+      })
+    );
+  }
+
+  getToken(){
+    return localStorage.getItem(this.tokenKey);
+  }
+  
+}
